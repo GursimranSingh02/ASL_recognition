3ac86422dea949d2728dbdd2d5c2a3525ca2b275
Gursimran Singh <144690560+GursimranSingh02@users.noreply.github.com>
2024-07-27 12:24:16 +0530

    Update ASL_recognition.py

--- ASL_recognition.py
+++ ASL_recognition.py
@@ -102,7 +102,7 @@ labels = ["A", "B", "C", "Calm down", "D", "E", "F", "G", "H", "Hello", "I", "I 
 class VideoProcessor(VideoProcessorBase):
     def __init__(self):
         self.detector = HandDetector(maxHands=1)
-        self.classifier = Classifier("Model_ASL/keras_model.h5", "Model_ASL/labels.txt")
+        self.classifier = Classifier("keras_model.h5", "labels.txt")
         self.offset = 20
         self.imgSize = 300
         self.labels = labels
